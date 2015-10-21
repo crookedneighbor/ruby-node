6d11e9db689032b177c3bcd4323c31aae1c74f4e
Blade Barringer <blade@crookedneighbor.com>
2015-10-21 06:07:08 -0500

    Use Guns and Ships lyrics instead

--- test/w.js
+++ test/w.js
@@ -5,10 +5,10 @@ import {
 describe('w', () => {
   describe('#w', () => {
     it('transforms words into an array', () => {
-      let words = 'these are some words';
+      let words = 'How does a ragtag volunteer army in need of a shower somehow defeat a global superpower?';
       let wordsArray = w(words);
 
-      expect(wordsArray).to.eql(['these', 'are', 'some', 'words']);
+      expect(wordsArray).to.eql(['How', 'does', 'a', 'ragtag', 'volunteer', 'army', 'in', 'need', 'of', 'a', 'shower', 'somehow', 'defeat', 'a', 'global', 'superpower?']);
     });
   });
 });
