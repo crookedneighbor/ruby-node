40602374080af8a2488cb9f942ea9b9b0d57ec26
Blade Barringer <blade@crookedneighbor.com>
2015-10-21 12:35:40 -0500

    Add index and documentation

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Ruby for Node
+// diamond
+// Helper methods from ruby ported to Node.
+
+// # Getting Started
+//
+// ```bash
+// npm install ruby --save
+// ```
+//
+// ```js
+// let r = require('ruby');
+//
+// let words = r.w('I am not throwing away my shot!');
+// words; // ['I', 'am', 'not', 'throwing', 'away', 'my', 'shot!']
+// ```
+
+// # Top Level Methods
+//
+// See [top level methods](top_level_methods.html)
+export * from './top_level_methods';
