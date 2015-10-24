6cf69413c53c5005be52f470eb08b6b8684d881c
Blade Barringer <blade@crookedneighbor.com>
2015-10-24 14:28:13 -0500

    Add clean task to babel to clean up old compiled files

--- tasks/gulp-babel.js
+++ tasks/gulp-babel.js
@@ -1,15 +1,20 @@
 import gulp from 'gulp';
+import del from 'del';
 import babel from 'gulp-babel';
 
 const SOURCE = 'src/**/*.js';
 const DIST = 'dist/';
 
-gulp.task('babel', () => {
+gulp.task('babel', ['babel:clean'], () => {
   return gulp.src(SOURCE)
     .pipe(babel({
       comments: false,
     }))
     .pipe(gulp.dest(DIST));
+});
+
+gulp.task('babel:clean', () => {
+  return del([DIST]);
 });
 
 gulp.task('babel:watch', () => {
